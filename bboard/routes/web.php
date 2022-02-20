2b11ff7161e4f918b9920d8c6821ad8e5afc1657
axmeshkov <axmeshkov@gmail.com>
2022-02-20 03:55:00 +0300

    used laravel ui auth

--- bboard/routes/web.php
+++ bboard/routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\BbsController::class, 'index'])->name('index');
 Route::get('/{bb}', [\App\Http\Controllers\BbsController::class, 'detail'])->name('detail');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
