c76ab1afd3fe6689d7b077f07b53daac44b8da72
axmeshkov <axmeshkov@gmail.com>
2022-02-21 04:46:02 +0300

    added validation of forms

--- bboard/app/Http/Controllers/HomeController.php
+++ bboard/app/Http/Controllers/HomeController.php
@@ -8,6 +8,19 @@ use App\Models\Bb;
 
 class HomeController extends Controller
 {
+    private const BB_VALIDATOR = [
+        'title' => 'required|max:50',
+        'content' => 'required',
+        'price' => 'required|numeric'
+    ];
+
+    private const BB_ERROR_MESSAGES = [
+        'price.required' => 'Раздавать товары бесплатно нельзя',
+        'required' => 'Заполните это поле',
+        'max' => 'Значение не должно быть длиннее :max символов',
+        'numeric' => 'Введите число'
+    ];
+
     /**
      * Create a new controller instance.
      *
@@ -33,7 +46,9 @@ class HomeController extends Controller
     }
 
     public function storeBb(Request $request){
-        Auth::user()->bbs()->create(['title' => $request->title, 'content' => $request->content, 'price' => $request->price]);
+        $validated = $request->validate(self::BB_VALIDATOR,
+                                        self::BB_ERROR_MESSAGES);
+        Auth::user()->bbs()->create(['title' => $validated['title'], 'content' => $validated['content'], 'price' => $validated['price']]);
         return redirect()->route('home');
     }
 
@@ -42,7 +57,9 @@ class HomeController extends Controller
     }
 
     public function updateBb(Request $request, Bb $bb) {
-        $bb->fill(['title' => $request->title, 'content' => $request->content, 'price' => $request->price]);
+        $validated = $request->validate(self::BB_VALIDATOR,
+            self::BB_ERROR_MESSAGES);
+        $bb->fill(['title' => $validated['title'], 'content' => $validated['content'], 'price' => $validated['price']]);
         $bb->save();
         return redirect()->route('home');
     }
--- bboard/resources/views/bb_edit.blade.php
+++ bboard/resources/views/bb_edit.blade.php
@@ -8,15 +8,30 @@
         @method('PATCH')
         <div class="form-group">
             <label for="txtTitle">Товар</label>
-            <input name="title" id="txtTitle" class="form-control" value="{{ $bb->title }}">
+            <input name="title" id="txtTitle" class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $bb->title) }}">
+            @error('title')
+            <span class="invalid-feedback">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="txtContent">Описание</label>
-            <textarea name="content" id="txtContent" class="form-control" rows="3">{{ $bb->content }}</textarea>
+            <textarea name="content" id="txtContent" class="form-control @error('content') is-invalid @enderror" rows="3">{{ old('content', $bb->content) }}</textarea>
+            @error('content')
+            <span class="invalid-feedback">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="txtPrice">Цена</label>
-            <textarea name="price" id="txtPrice" class="form-control">{{ $bb->price }}</textarea>
+            <textarea name="price" id="txtPrice" class="form-control @error('price') is-invalid @enderror">{{ old('price',$bb->price) }}</textarea>
+            @error('price')
+            <span class="invalid-feedback">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
         </div>
         <input type="submit" class="btn btn-primary" value="Сохранить">
     </form>
