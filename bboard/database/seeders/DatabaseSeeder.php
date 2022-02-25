5da0bc4c7099f93372f7d0b514cb86789ca976ad
axmeshkov <axmeshkov@gmail.com>
2022-02-25 09:12:44 +0300

    seeding database

--- bboard/database/seeders/DatabaseSeeder.php
+++ bboard/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $password = Hash::make('user');
+        for ($i = 0; $i < 3; $i++) {
+            User::create(['name' => Str::random(10),
+                'email' => Str::random(10) . '@bboard.ru',
+                'password' => $password
+            ]);
+        }
     }
 }
