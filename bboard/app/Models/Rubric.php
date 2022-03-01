8b9f0ed795eabd7858bd0a04102ef0daea977581
axmeshkov <axmeshkov@gmail.com>
2022-03-01 14:05:38 +0300

    added accessor and mutator

--- bboard/app/Models/Rubric.php
+++ bboard/app/Models/Rubric.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Rubric extends Model
 {
     protected $fillable = ['name'];
 
-    public function bbs() {
+    public function bbs()
+    {
         return $this->hasMany(Bb::class);
     }
+
+    public function getNameAttribute($value)
+    {
+        return Str::ucfirst($value);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::lower($value);
+    }
+
 }
